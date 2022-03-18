08fc98a9201927d596dafad8a94c443327c9de9e
renatoas77 <renato.antonio7@hotmail.com>
2022-03-17 22:19:01 -0300

    other entities and second migration

--- SaleWebMVC/Data/SaleWebMVCContext.cs
+++ SaleWebMVC/Data/SaleWebMVCContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.EntityFrameworkCore;
 using SaleWebMVC.Models;
 
@@ -14,6 +11,9 @@ namespace SaleWebMVC.Data
         {
         }
 
-        public DbSet<SaleWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        
     }
 }
--- SaleWebMVC/Migrations/20220318011506_OtherEntities.Designer.cs
+++ SaleWebMVC/Migrations/20220318011506_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaleWebMVC.Data;
 
 namespace SaleWebMVC.Migrations
 {
     [DbContext(typeof(SaleWebMVCContext))]
-    partial class SaleWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20220318011506_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
