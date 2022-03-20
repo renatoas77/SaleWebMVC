0de5b8439b9f5885fbfcdf078a4855881178b8ab
renatoas77 <renato.antonio7@hotmail.com>
2022-03-20 17:11:52 -0300

    Foreign key not null

--- SaleWebMVC/Migrations/20220320200450_DepartmentForeignKey.Designer.cs
+++ SaleWebMVC/Migrations/20220320200450_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaleWebMVC.Data;
 
 namespace SaleWebMVC.Migrations
 {
     [DbContext(typeof(SaleWebMVCContext))]
-    partial class SaleWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20220320200450_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SaleWebMVC/Services/SellerService.cs
+++ SaleWebMVC/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SaleWebMVC.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
