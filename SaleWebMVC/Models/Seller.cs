1de7cee64091800d0fa84bbdc14f52fd58d28403
renatoas77 <renato.antonio7@hotmail.com>
2022-03-23 22:04:40 -0300

    Validation

--- SaleWebMVC/Models/Seller.cs
+++ SaleWebMVC/Models/Seller.cs
@@ -8,15 +8,22 @@ namespace SaleWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60,MinimumLength =3,ErrorMessage = "{0} size should be between {2} and {1} characters")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.00,50000.00,ErrorMessage = "{0} must bem from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
